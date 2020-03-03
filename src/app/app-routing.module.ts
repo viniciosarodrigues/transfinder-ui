5f483684c25d15895753b62af30f285ce8056bc6
viniciosarodrigues <viniciosarodrigues@gmail.com>
2020-03-03 19:26:46 -0300

    Adicionando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EditOrCreateComponent } from './edit-or-create/edit-or-create.component';
 import { HomeComponent } from './home/home.component';
+import { CarriersComponent } from './carriers/carriers.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'carriers', component: CarriersComponent },
   { path: 'edit-or-create', component: EditOrCreateComponent },
 ];
 
